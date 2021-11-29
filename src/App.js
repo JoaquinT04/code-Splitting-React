c1567fd922ac66e33d4e069c333b809df8011759
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2021-11-29 11:46:13 -0300

    Imports Dinámicos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Surprise from './Surprise';
 
 function App() {
   const [showSurprise, setShowSurprise] = useState(false);
 
+  // import dinamico me carga en un chuck.js aparte todo el codigo de surprise
+  /*useEffect( () => {
+    // import dinamico 
+    import("./hello")
+      .then(mod => mod.default())
+      /* Lo que tenemos como resultado de la promesa es el modulo, y la propiedad default me trae lo que exportamos por default 
+  },[]);
+  */
   return (
     <div>
       <button onClick = { () => setShowSurprise(true) }>Mostrat sorpresa</button>
